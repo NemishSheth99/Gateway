a4fad5746e6a5d3c62f2b7ce071553e524254d43
NemishSheth99 <74645365+NemishSheth99@users.noreply.github.com>
2021-03-03 17:48:13 +0530

    Testing Custom Extension Methods

---
+++ TestingAssignment/ExtensionMethods/StringExtensions.Test/StringUtilityTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace StringExtensions.Test
+{
+    public class StringUtilityTest
+    {
+        [Theory]
+        [InlineData("Nemish","nEMISH")]
+        [InlineData("Nemish Sheth", "nEMISH sHETH")]
+        [InlineData("Nemish @#$ Sheth", "nEMISH @#$ sHETH")]
+        public void TetsCaseConversion( string inpStr,string expected)
+        {
+            Assert.Equal(expected, inpStr.CaseConversion());
+        }
+        [Theory]
+        [InlineData("Nemish", "Nemish")]
+        [InlineData("Nemish ShEth", "Nemish Sheth")]
+        [InlineData("neMISh @#$ SHETH", "Nemish @#$ SHETH")]
+        public void TetstTitleCase(string inpStr, string expected)
+        {
+            Assert.Equal(expected, inpStr.TitleCase());
+        }
+
+        [Theory]
+        [InlineData("Nemish", false)]
+        [InlineData("Nemish ShEth", false)]
+        [InlineData("nemish nemish", true)]
+        public void TestIsLower(string inpStr, bool expected)
+        {
+            Assert.Equal(expected, inpStr.IsLower());
+        }
+
+        [Theory]
+        [InlineData("Nemish", "Nemish")]
+        [InlineData("Nemish ShEth", "Nemish Sheth")]
+        [InlineData(" neMISh @#$ SHETH", " Nemish @#$ Sheth")]
+        public void TestCapitalCase(string inpStr, string expected)
+        {
+            Assert.Equal(expected, inpStr.CapitalCase());
+        }
+        [Theory]
+        [InlineData("NEMISH", true)]
+        [InlineData("Nemish ShEth", false)]
+        [InlineData("NEMISH SHETH", true)]
+        public void TestIsUpper(string inpStr, bool expected)
+        {
+            Assert.Equal(expected, inpStr.IsUpper());
+        }
+
+        [Theory]
+        [InlineData("4232",true)]
+        [InlineData("87890", true)]
+        [InlineData("13da33", false)]
+        public void TestIsDigit(string inpStr, bool expected)
+        {
+            Assert.Equal(expected, inpStr.ToDigitPossible());
+        }
+
+        [Theory]
+        [InlineData("Nemish", "Nemis")]
+        [InlineData("Nemish ShEth  ", "Nemish ShEth ")]
+        [InlineData("neMISh @#$", "neMISh @#")]
+        public void TestRemoveLastChar(string inpStr, string expected)
+        {
+            Assert.Equal(expected, inpStr.RemoveLastChar());
+        }
+
+        [Theory]
+        [InlineData("ab bc cd", 3)]
+        [InlineData("aa bb cc d d", 5)]
+        [InlineData("neMISh @#$", 2)]
+        public void TestWordCount(string inpStr, int expected)
+        {
+            Assert.Equal(expected, inpStr.CountWord());
+        }
+
+        [Theory]
+        [InlineData("4232", 4232)]
+        [InlineData("87890", 87890)]
+        public void TestToInt(string inpStr, int expected)
+        {
+            Assert.Equal(expected, inpStr.ToInt());
+        }
+    }
+}
---
+++ TestingAssignment/ExtensionMethods/StringExtensions/StringUtility.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace StringExtensions
+{
+    public static class StringUtility
+    {
+        /// <summary>
+        /// Uppercase characters converted to lowercase and vice versa
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>string</returns>
+        public static string CaseConversion(this string str)
+        {
+            if (str.Length > 0)
+            {
+                
+                char[] alphabetArray = str.ToCharArray();
+                for(int i=0; i<alphabetArray.Length; i++)
+                {
+                   
+                    alphabetArray[i] = char.IsUpper(alphabetArray[i]) ? char.ToLower(alphabetArray[i]) : char.ToUpper(alphabetArray[i]);
+                }
+                return new string(alphabetArray);
+            }
+            else
+                return str;
+        }
+
+        /// <summary>
+        /// Converts to title case 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>string</returns>
+        public static string TitleCase(this string str)
+        {
+            if (str == null) return null;
+            else return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str);
+        }
+
+        /// <summary>
+        /// String is in lower case or not
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>bool</returns>
+        public static bool IsLower(this string str)
+        {
+            if (str.Length == 0) return false;
+            else
+            {
+                char[] alphabetArray = str.ToCharArray();
+                for(int i=0; i<alphabetArray.Length; i++)
+                {
+                    char alphabet = alphabetArray[i];
+                    if( !(char.IsWhiteSpace(alphabet) || char.IsLower(alphabet)) )
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Returns a capitalized version of given input string 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>string</returns>
+        public static string CapitalCase(this string str)
+        {
+            if (str.Length > 0)
+            {
+                char[] alphabetArray = str.ToCharArray();
+
+                if (char.IsLower(alphabetArray[0])) char.ToUpper(alphabetArray[0]);
+
+                for (int i = 1; i < alphabetArray.Length; i++)
+                {
+                    if (char.IsWhiteSpace(alphabetArray[i - 1]))
+                    {
+                        alphabetArray[i] = char.ToUpper(alphabetArray[i]);
+                        i++;
+                    }
+                    while ( i < alphabetArray.Length && !char.IsWhiteSpace(alphabetArray[i]))
+                    {
+                        //if (char.IsLetter(alphabetArray[i]))
+                        //{
+                            alphabetArray[i] = char.ToLower(alphabetArray[i]);
+                            i++;
+                        //}
+                    }
+                }
+                return new string(alphabetArray);
+            }
+            else
+                return str;
+        }
+
+        /// <summary>
+        /// String is in upper case or not
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>bool</returns>
+        public static bool IsUpper(this string str)
+        {
+            if (str.Length == 0) return false;
+            else
+            {
+                char[] alphabetArray = str.ToCharArray();
+                for (int i = 0; i < alphabetArray.Length; i++)
+                {
+                    char alphabet = alphabetArray[i];
+                    if (!(char.IsWhiteSpace(alphabet) || char.IsUpper(alphabet)))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// string can be converted to a valid numeric value or not
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>bool</returns>
+        public static bool ToDigitPossible(this string str)
+        {
+            if (str.Length == 0) return false;
+            else
+            {
+                char[] alphabetArray = str.ToCharArray();
+                for (int i = 0; i < alphabetArray.Length; i++)
+                {
+                    if (!char.IsDigit(alphabetArray[i]))
+                        return false;
+                }
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// remove the last character from given the string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>string</returns>
+        public static string RemoveLastChar(this string str)
+        {
+            if(str.Length == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return str.Substring(0, str.Length - 1);
+            }
+        }
+
+        /// <summary>
+        ///word count from an input string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>int</returns>
+        public static int CountWord(this string str)
+        {
+            if (str.Length == 0) return 0;
+            else
+            {
+                string[] wordArray = str.Split(' ');
+                return wordArray.Count();
+            }
+        }
+
+        /// <summary>
+        /// Convert an input string to integer
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>int</returns>
+        public static int ToInt(this string str)
+        { int x = 0;
+           
+            if(str.Length>0)
+            {
+                Int32.TryParse(str, out x);
+            }
+            return x;
+
+        }
+    }
+
+}
